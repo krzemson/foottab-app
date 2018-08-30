f67492045ee5995f5c75e184b5a27be0f8657281
krzemson <krzeminiak10@gmail.com>
2018-08-30 09:16:10 +0200

    change structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pilkarzyki</title>
+</head>
+
+<body>
+   
+   <form method="post" action="players.php">
+      <label for="number">Podaj ilość graczy:<br>
+       <input type="number" name="number" min="4"><br><br></label>
+       <input type="submit" name="submit">
+   </form>
+    
+</body>
+</html>
---
+++ players.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if (isset($_POST['submit']) & !empty($_POST['number'])) {
+    $number = $_POST['number'];
+    
+     $_SESSION['number'] = $number;
+} else {
+    header("Location: index.php");
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pilkarzyki</title>
+</head>
+
+<body>
+   
+   <form method="post" action="shuffle.php">
+        <?php
+        for ($i = 1; $i <= $number; $i++) { ?>
+      <label for="nick">Podaj nazwę gracza:
+       <input type="text" name="nick<?php echo $i?>">
+      </label><br><br>
+       
+        <?php } ?>
+       
+       <input type="submit" name="submit">
+   </form>
+    
+</body>
+</html>
